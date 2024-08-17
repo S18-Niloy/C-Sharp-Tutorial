6fbc02bdc43750f47e41e90e9e69fe37d7aaa10f
Soumik Deb Niloy <80791375+S18-Niloy@users.noreply.github.com>
2024-08-17 08:57:26 -0700

    Update 03_If_else_condition.cs

--- Basic/03_If_else_condition.cs
+++ Basic/03_If_else_condition.cs
@@ -1,1 +1,24 @@
 
+using System;
+
+namespace LeapYearCheck
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a year:");
+            int year = Convert.ToInt32(Console.ReadLine());
+            
+            if ((year % 4 == 0) && (year % 100 != 0) || (year % 400 == 0))
+            {
+                Console.WriteLine(year + " is a leap year.");
+            }
+            else
+            {
+                Console.WriteLine(year + " is not a leap year.");
+            }
+        }
+    }
+}
+
