c3836818e92538760a5ec8ba30af736fd9f4c1d7
Soumik Deb Niloy <80791375+S18-Niloy@users.noreply.github.com>
2024-08-17 09:25:44 -0700

    Create 04_Loop.cs

---
+++ Basic/04_Loop.cs
@@ -0,0 +1,37 @@
+// While loop
+using System;
+
+namespace Myapp{
+
+    class loop{
+    
+        static void Main(string[] args){
+        
+        int i = 0;
+        while (i<5){
+            Console.WriteLine(i);
+            i+=1;
+        }
+        
+        }
+    
+    }
+}
+
+// For loop
+
+using System;
+
+namespace MyApplication
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      for (int i = 0; i<5; i++) 
+      {
+        Console.WriteLine(i);
+      }
+    }
+  }
+}
