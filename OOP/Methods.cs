68a8eb4d5f2365352c1fb5513396a14671e8db89
Soumik Deb Niloy <80791375+S18-Niloy@users.noreply.github.com>
2024-08-20 09:36:49 -0700

    Update Methods.cs

--- OOP/Methods.cs
+++ OOP/Methods.cs
@@ -17,3 +17,90 @@ namespace MyApplication
     }
   }
 }
+
+////// Parameters and Arguments
+
+using System;
+
+namespace MyApplication
+{
+  class Program
+  {
+    static void MyMethod(string fname, int age)
+    {
+      Console.WriteLine(fname + " is " + age);
+    }
+
+    static void Main(string[] args)
+    {
+      MyMethod("Liam", 5);
+      MyMethod("Jenny", 8);
+      MyMethod("Anja", 31);
+    }
+  }
+}
+
+///// Default parameters
+
+using System;
+
+namespace MyApplication
+{
+  class Program
+  {
+    static void MyMethod(string country = "Norway")
+    {
+      Console.WriteLine(country);
+    }
+
+    static void Main(string[] args)
+    {
+      MyMethod("Sweden");
+      MyMethod("India");
+      MyMethod();
+      MyMethod("USA");
+    }
+  }
+}
+
+
+/////// Return values 
+
+using System;
+
+namespace MyApplication
+{
+  class Program
+  {
+    static int MyMethod(int x, int y)
+    {
+      return x + y;
+    }
+
+    static void Main(string[] args)
+    {
+      int z = MyMethod(5, 3);
+      Console.WriteLine(z);
+    }
+  }
+}
+
+///// Named arguments
+
+using System;
+
+namespace MyApplication
+{
+  class Program
+  {
+    static void MyMethod(string child1, string child2, string child3)
+    {
+      Console.WriteLine("The youngest child is: " + child3);
+    }
+
+    static void Main(string[] args)
+    {
+      MyMethod(child3: "John", child1: "Liam", child2: "Liam");
+    }
+  }
+}
